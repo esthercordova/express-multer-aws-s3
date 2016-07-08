57767cdbcc9746a6a079a9d477c6bd33f4a800d1
esthercordova <estherpiacordova@gmail.com>
2016-07-08 15:58:35 -0400

    Use JavaScript for uploadeing files
    Use $.ajax instead of the browsers own POST for forms

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,33 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+$(() => {
+  $('#application-x-www-form-data').on('submit', (event) => {
+    let getFormFields = require('../../lib/get-form-fields');
+
+    event.preventDefault();
+
+    $.ajax({
+      method: 'POST',
+      url: 'http://httpbin.org/post',
+      data: getFormFields(event.target),
+    })
+    .done((data) => console.log(data))
+    .fail((fail) => console.error(error));
+  });
+
+  $('#multipart-form-data').on('submit', (event) => {
+    event.preventDefault();
+
+    $.ajax({
+      method: 'POST',
+      url: 'http://httpbin.org/post',
+      data: new FormData(event.target),
+      ContentType: false,
+      processData: false,
+    })
+    .done((data) => console.log(data))
+    .fail((fail) => console.error(error));
+  });
+});
